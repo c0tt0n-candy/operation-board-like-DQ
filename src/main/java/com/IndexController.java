aa29120d00d758792763c160dc6b9ee6392f2574
Yutaka Okubo <yutaka@Yutaka-Air.local>
2016-03-10 10:07:43 +0900

    /operation追加

--- src/main/java/com/IndexController.java
+++ src/main/java/com/IndexController.java
@@ -5,13 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-    @RequestMapping(value="/")
-    public String index() {
-        return "index";
-    }
-    
-	@RequestMapping(value="/today")
+	@RequestMapping(value = "/")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping(value = "/today")
 	public String main() {
+		return "today";
+	}
+
+	@RequestMapping(value = "/operation")
+	public String operation() {
 		//System.out.println("okubo");
 		return "today";
 	}
