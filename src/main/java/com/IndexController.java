94f3c9ce5ce0bc9ac2e2b6e3d8e08943cf6a6b36
Yutaka Okubo <yutaka@Yutaka-Air.local>
2016-03-23 14:48:45 +0900

    mainページを/で表示する

--- src/main/java/com/IndexController.java
+++ src/main/java/com/IndexController.java
@@ -6,17 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 	@RequestMapping(value = "/")
-	public String index() {
-		return "index";
-	}
-
-	@RequestMapping(value = "/today")
 	public String main() {
 		return "today";
-	}
-
-	@RequestMapping(value = "/operation")
-	public String operation() {
-		return "operation";
 	}
 }
