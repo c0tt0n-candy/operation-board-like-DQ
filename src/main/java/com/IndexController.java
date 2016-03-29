b86f4da5503cff8b6dc1ae105eb88aec054be22f
Yutaka Okubo <yutaka@Yutaka-Air.local>
2016-03-29 11:38:58 +0900

    URLを修正

--- src/main/java/com/IndexController.java
+++ src/main/java/com/IndexController.java
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 	@RequestMapping(value = "/")
-	public String main() {
+	public String index() {
+		return "today";
+	}
+
+	@RequestMapping(value = "/today")
+	public String today() {
 		return "today";
 	}
 }
