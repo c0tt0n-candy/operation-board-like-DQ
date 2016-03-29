03426a314fda41174bfd53facb014079e9d0754d
Yutaka Okubo <yutaka@Yutaka-Air.local>
2016-03-29 17:38:57 +0900

    テーブルのupdate()をoperation_history_Tblに合わせて修正

--- src/main/java/com/IndexController.java
+++ src/main/java/com/IndexController.java
@@ -1,17 +1,44 @@
 package com;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
+
+	// @Autowired
+	// OperationManager operationManager;
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
 	@RequestMapping(value = "/")
-	public String index() {
+	public String index(Model model) {
+
+		List<Operation> ope = jdbcTemplate.query("select number,content from operation_List",
+				(rs, rowNum) -> new Operation(rs.getInt("number"), rs.getString("content")));
+
+		model.addAttribute("ope", ope);
 		return "today";
 	}
 
 	@RequestMapping(value = "/today")
-	public String today() {
+	public String update(Model model, @RequestParam("number") String number) {
+
+		String content = jdbcTemplate.queryForObject("select content from operation_List where number=?", String.class,
+				number);
+
+		jdbcTemplate.update("insert into operation_history_Tbl(content,created) values(?,?)", content,
+				new Timestamp(System.currentTimeMillis()));
+		
+		model.addAttribute("number",number);
 		return "today";
 	}
 }
