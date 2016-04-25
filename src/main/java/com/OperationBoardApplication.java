23eccf2c6c6995a4844e5a6e5d2ac924bffd5fdb
y_okubo <y_okubo>
2016-04-25 10:57:53 +0900

    for Heroku

--- src/main/java/com/OperationBoardApplication.java
+++ src/main/java/com/OperationBoardApplication.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
+@ComponentScan
 public class OperationBoardApplication implements CommandLineRunner {
 
 	public static void main(String args[]) throws Exception {
